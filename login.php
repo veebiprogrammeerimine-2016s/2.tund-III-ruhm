5db183b8bc673c4bfd497b7c3a1ad424b791c976
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-09-15 12:41:55 +0300

    added validation

--- login.php
+++ login.php
@@ -1,3 +1,26 @@
+<?php 
+
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump($_POST);
+	
+	$signupEmailError = "";
+	
+	//kas on üldse olemas
+	if (isset ($_POST["signupEmail"])) {
+		
+		// oli olemas, ehk keegi vajutas nuppu
+		// kas oli tühi
+		if (empty ($_POST["signupEmail"])) {
+			
+			//oli tõesti tühi
+			$signupEmailError = "See väli on kohustuslik";
+			
+		}
+		
+	}
+	
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,17 +30,36 @@
 
 		<h1>Logi sisse</h1>
 		
-		<form>
+		<form method="POST">
 			
-			<input type="email">
+			<label>E-post</label><br>
+			<input name="loginEmail" type="email">
 			
 			<br><br>
 			
-			<input type="password">
-			
+			<label>Parool</label><br>
+			<input name="loginPassword" type="password">
+						
 			<br><br>
 			
 			<input type="submit">
+		
+		</form>
+		
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST">
+			
+			<label>E-post</label><br>
+			<input name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
+			
+			<input placeholder="Parool" name="signupPassword" type="password">
+						
+			<br><br>
+			
+			<input type="submit" value="Loo kasutaja">
 		
 		</form>
 
